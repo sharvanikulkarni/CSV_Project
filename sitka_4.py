1392e6d1a97e09022387745c546f5c3e92aa266d
Sharvani Kulkarni <sharvani_kulkarni1@baylor.edu>
2022-03-15 14:44:25 -0500

    sitka 4 updated

--- sitka_4.py
+++ sitka_4.py
@@ -1,24 +1,19 @@
-#1) changing the file to include all the data for the year 2018
-#2) change the title to - Daily low and high temp 2018
-#3) extract low temps from the file and add to chart
-#4) share in the area between low and high
+# 1) Changing the file to include all the data for the year of 2018
+# 2) Change the title to - Daily low and high temperatures - 2018
+# 3) Extract low temps from the file and add to chart
+# 4) Shade in the area between high and low
 
 
 import csv
 from datetime import datetime
-from email import header
-import re
+from numpy import append
 
-open_file = open('death_valley_2018_simple.csv','r')
+infile = open("death_valley_2018_simple.csv", 'r')
+file_reader = csv.reader(infile, delimiter = ",")
 
+header_row = next(file_reader)
 
-csv_file = csv.reader(open_file, delimiter=',')
-
-
-#to skip a line if the file contains a header record
-header_row = next(csv_file)
-
-print(type(header_row))
+#print(type(header_row))
 
 for index, column_header in enumerate(header_row):
     print(index, column_header)
@@ -27,55 +22,46 @@ for index, column_header in enumerate(header_row):
 highs = []
 dates = []
 lows = []
-#test_date = datetime.strptime('2018-07-01', '%Y-%m-%d')
-#print(test_date)
+
+test_date = datetime.strptime('2018-07-01','%Y-%m-%d')
+print(test_date)
 
 
-for row in csv_file:
+
+for item in file_reader:
+
     try:
-        current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = (int(row[4]))
-        low = (int(row[4]))
-        
-
-        
-    except ValueError:  
-        print(f"missing data for {current_date}")
+        current_date = datetime.strptime(item[2],'%Y-%m-%d')
+        high = int(item[4])
+        low = int(item[5])
+       
+   
+    except ValueError:
+        print(f"Missing data for {current_date}")
 
     else:
         highs.append(high)
         lows.append(low)
         dates.append(current_date)
+   
+   
+print(highs)
+print(dates)
 
-#print(highs)
-#print(dates)
-#print(lows)
 
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
 
-
-plt.plot(dates,highs, c="red")
-plt.plot(dates,lows, c='green')
-
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
-
-plt.title("Daily low and high temperatures, 2018", fontsize=16)
+plt.plot(dates, highs, c="red")
+plt.plot(dates, lows, c="blue")
+plt.fill_between(dates, lows, highs, facecolor='blue', alpha=0.1)
+plt.title("Daily low and high temperatures - 2018", fontsize=16)
 plt.xlabel("Year 2018")
 plt.ylabel("Temperatures (F)", fontsize=16)
 plt.tick_params(axis="both", which="major", labelsize=16)
 
+
 fig.autofmt_xdate()
 
-plt.show()
-
-plt.subplot(2, 1, 1)
-plt.plot(dates, highs, c="red")
-plt.title("highs")
-plt.subplot(2,1,2)
-plt.plot(dates, lows, c="blue")
-plt.title("lows")
-
-plt.suptitle("Highs and lows of 2018")
 plt.show()
